a3a6f80f783af9d96b04e0961b02af09be859fc5
Aeen <139196129+Aeendgh@users.noreply.github.com>
2025-11-06 02:09:02 +0330

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -39,6 +39,14 @@ const projectsData: Project[] = [
         githubLink: "#",
         category: "Script"
     },
+    {
+        name: "SAP HANA Database Presentation",
+        description: "An explanatory blog about sap hana in-memory data base",
+        techStack: ["SAP", "HANA", "Database"],
+        image: "https://cloudfoundation.com/blog/wp-content/uploads/2023/03/SAP-HANA-is-versatile..png",
+        githubLink: "./hana-present.html",
+        category: "Presentation"
+    },
 ];
 
 const ProjectCard: React.FC<{ project: Project }> = ({ project }) => (
